3e1991552e1a2c61dd15e6c1bf0139126df78847
zhanghehe <hezhanghe@126.com>
2020-02-13 12:26:24 +0800

    task - 不停旋转

--- app/src/main/java/com/example/opengl2/view/GL2View.kt
+++ app/src/main/java/com/example/opengl2/view/GL2View.kt
@@ -5,6 +5,7 @@ import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
 import android.util.AttributeSet
+import android.util.Log
 import com.example.opengl2.shape.Triangle
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -19,24 +20,46 @@ class GL2View(context: Context, attrs: AttributeSet? = null) : GLSurfaceView(con
 
     init {
         setEGLContextClientVersion(2)
+        //该模式会导致 onDrawFrame 不断绘制，绘制间隔在12~50ms 之间
         mRender = GL2Renderer()
         setRenderer(mRender)
     }
 
     inner class GL2Renderer : GLSurfaceView.Renderer {
 
+        private val TAG = GL2Renderer::class.java.simpleName
+
+        private var currDeg = 30
         //视图投影矩阵
-        private var mMVPMatrix=FloatArray(16)
+        private var mMVPMatrix = FloatArray(16)
         //投影矩阵
         private var mProjectionMatrix = FloatArray(16)
         //视图矩阵
         private var mViewMatrix = FloatArray(16)
+        //变换矩阵
+        private var mOpMatrix = FloatArray(16)
         private lateinit var mTriangle: Triangle
 
         override fun onDrawFrame(gl: GL10?) {
+            Log.d(TAG, "currDeg $currDeg")
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
-            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0)
+            Matrix.setRotateM(mOpMatrix, 0, currDeg.toFloat(), 0f, 0f, -1f)
+            Matrix.multiplyMM(
+                mMVPMatrix, 0,
+                mViewMatrix, 0,
+                mOpMatrix, 0
+            )
+            Matrix.multiplyMM(
+                mMVPMatrix, 0,
+                mProjectionMatrix, 0,
+                mMVPMatrix, 0
+            )
             mTriangle.draw(mMVPMatrix)
+
+            currDeg++
+            if (currDeg >= 360) {
+                currDeg = 0
+            }
         }
 
         override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
