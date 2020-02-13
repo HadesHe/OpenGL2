23adb8ebe3e95301e5507712e40e270a889dd46a
zhanghehe <hezhanghe@126.com>
2020-02-13 10:45:44 +0800

    task - 顶点着色

--- app/src/main/assets/tri.vert
+++ app/src/main/assets/tri.vert
@@ -1,4 +1,8 @@
-attribute vec4 vPosition;
+attribute vec3 vPosition;
+//uniform mat4 uMVPMatrix;//变换矩阵
+attribute vec4 aColor;//顶点颜色
+varying vec4 vColor;//片元颜色
 void main(){
-    gl_Position=vPosition;
+    gl_Position=vec4(vPosition, 1);
+    vColor=aColor;
 }
--- app/src/main/java/com/example/opengl2/shape/Triangle.kt
+++ app/src/main/java/com/example/opengl2/shape/Triangle.kt
@@ -13,6 +13,7 @@ import java.nio.FloatBuffer
  */
 class Triangle(val context: Context) {
 
+    private var mColorBuffer: FloatBuffer
     private var mColorHandle: Int = 0
     private var mPositionHandle: Int = 0
     private var mProgram: Int = 0
@@ -24,8 +25,13 @@ class Triangle(val context: Context) {
         vertexBuffer = bb.asFloatBuffer()
         vertexBuffer.put(sCoor)
         vertexBuffer.position(0)
-        val vertexShader = GLUtil.loadShaderAssets(context,GLES20.GL_VERTEX_SHADER, "tri.vert")
-        val fragmentShader = GLUtil.loadShaderAssets(context,GLES20.GL_FRAGMENT_SHADER, "tri.frag")
+
+        mColorBuffer = ByteBuffer.allocateDirect(colors.size * 4).order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+        mColorBuffer.put(colors)
+        mColorBuffer.position(0)
+        val vertexShader = GLUtil.loadShaderAssets(context, GLES20.GL_VERTEX_SHADER, "tri.vert")
+        val fragmentShader = GLUtil.loadShaderAssets(context, GLES20.GL_FRAGMENT_SHADER, "tri.frag")
         mProgram = GLES20.glCreateProgram()
         GLES20.glAttachShader(mProgram, vertexShader)
         GLES20.glAttachShader(mProgram, fragmentShader)
@@ -41,10 +47,16 @@ class Triangle(val context: Context) {
             GLES20.GL_FLOAT, false, vertexStride, vertexBuffer
         )
 
-        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor")
-        GLES20.glUniform4fv(mColorHandle, 1, color, 0)
+        mColorHandle = GLES20.glGetAttribLocation(mProgram, "aColor")
+        GLES20.glEnableVertexAttribArray(mColorHandle)
+        GLES20.glVertexAttribPointer(
+            mColorHandle, COLOR_PER_VERTEX,
+            GLES20.GL_FLOAT, false, vertexColorStride, mColorBuffer
+        )
+
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
         GLES20.glDisableVertexAttribArray(mPositionHandle)
+        GLES20.glDisableVertexAttribArray(mColorHandle)
     }
 
     companion object {
@@ -54,13 +66,18 @@ class Triangle(val context: Context) {
             1f, -1f, 0f
         )
 
-        val color = floatArrayOf(
-            0.63671875f, 0.76953125f, 0.22265625f, 1.0f
+        val colors = floatArrayOf(
+            1f, 1f, 0f, 1f,
+            0.05882353f, 0.09411765f, 0.9372549f, 1f,
+            0.19607843f, 1.0f, 0.02745098f, 1.0f
         )
         val COORD_PER_VERTEX = 3
 
+        val COLOR_PER_VERTEX = 4
+
         val vertexCount = sCoor.size / COORD_PER_VERTEX
         val vertexStride = COORD_PER_VERTEX * 4
+        val vertexColorStride = COLOR_PER_VERTEX * 4
 
 
     }
